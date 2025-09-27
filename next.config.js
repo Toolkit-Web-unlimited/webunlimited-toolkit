203fe8c56815d8bb31276bdddba8965859a3f4a1
webunlimitedch-ux <webunlimited.ch@gmail.com>
2025-09-27 17:54:59 +0200

    Fix Vercel deployment: ESLint config, Next.js config, build scripts

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
-    domains: [],
     dangerouslyAllowSVG: true,
     contentDispositionType: 'attachment',
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
